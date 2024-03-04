570a0abc98c48c58b5b6e19d5b5a87c224a74a8e
조영민 <105192448+Youngnn@users.noreply.github.com>
2024-03-04 20:48:43 +0900

    Add files via upload

--- echoserver.py
+++ echoserver.py
@@ -1,28 +1,28 @@
 # Packages
-import logging
-from telegram import Update
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
+import logging #화면에 로그를 보여주기 위함
+from telegram import Update #사용자가 텔레그램 서버에서 메세지 받을 때의 데이터
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext #텔레그램 확장 서비스 제공
 # Enable logging
 logging.basicConfig(
 format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO
-)
+) #INFO를 로깅하는 format을 제공하여 출력
 
-logger = logging.getLogger(__name__)
+logger = logging.getLogger(__name__) # 해당 파일의 로그를 받아서 그려줘라.
 
-# Message Handlers
+# Message Handlers 3가지
 def start(update: Update, context: CallbackContext) -> None:
     """send a message when the command /start is issued."""
     update.message.reply_text('start')
-    print(update.message.chat_id)
+    print(update.message.chat_id) # 유저 저장(chatid로 보낸다.)
     # 1854165413
 
 def help_command(update: Update, context: CallbackContext) -> None:
     """send a message when the command /help is issued."""
-    update.message.reply_text('Help!')
+    update.message.reply_text('Help!') #helf시에 데이터를 보여주는 것
 
 def echo(update: Update, context: CallbackContext) -> None:
     """Echo the user message."""
-    update.message.reply_text(update.message.text)
+    update.message.reply_text(update.message.text) #echoserver 생성(사용자가 입력한 데이터를 보내줌)
 
 def calc1(update: Update, context: CallbackContext) -> None:
     print(update.message.text)
@@ -46,21 +46,21 @@ def calc1(update: Update, context: CallbackContext) -> None:
 def main() -> None:
     """Start the bot."""
     # Create the Updater and pass it your bot's token.
-    updater = Updater("YOUR TOKEN")
+    updater = Updater("YOUR TOKEN") #token 가져오기
 
     #updater.bot.send_message(your_chat_id, "cbchoi_bot started")
 
-    # Get the dispatcher to register handlers
-    dispatcher = updater.dispatcher
-    # on different commands - answer in Telegram
-    dispatcher.add_handler(CommandHandler("start", start))
+    # 핸들러를 등록하기 위해 디스패처를 가져온다.
+    dispatcher = updater.dispatcher 
+    # 다른 명령에 대해서 Telegram이 답변
+    dispatcher.add_handler(CommandHandler("start", start)) #start끼리 연결하라 (디스패쳐와 핸들러)
     dispatcher.add_handler(CommandHandler("help", help_command))
     dispatcher.add_handler(CommandHandler("calc", calc1))
     
-    # on non command i.e message - echo the message on Telegram
+    # 메세지와 커맨드를 빼고 텔레그램에 메시지를 에코처리.
     dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, echo))
      # Start the Bot
-    updater.start_polling()
+    updater.start_polling() #백그라운드에서 블로킹하기 위에 idle까지 쓴다.(잠깐 멈춰주게)
     # Run the bot until you press Ctrl-C or the process receives SIGINT,
     # SIGTERM or SIGABRT. This should be used most of the time, since
     # start_polling() is non-blocking and will stop the bot gracefully.
